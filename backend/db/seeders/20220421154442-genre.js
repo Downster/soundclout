f7b1dba7c62ae5893550ce48d16991509a42a818
Downster <b.downing.code@gmail.com>
2022-04-29 09:59:56 -0700

    All db features working

--- backend/db/seeders/20220421154442-genre.js
+++ backend/db/seeders/20220421154442-genre.js
@@ -5,94 +5,154 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Genres', [
       {
-        name: 'None'
+        name: 'None',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
-        name: 'Alternative Rock'
+        name: 'Alternative Rock',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
-        name: 'Ambient'
+        name: 'Ambient',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
-        name: 'Classical'
+        name: 'Classical',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
-        name: 'Country'
+        name: 'Country',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
-        name: 'Dance & EDM'
+        name: 'Dance & EDM',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
-        name: 'Dancehall'
+        name: 'Dancehall',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
-        name: 'Deep House'
+        name: 'Deep House',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
-        name: 'Disco'
+        name: 'Disco',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
-        name: 'Drum & Bass'
+        name: 'Drum & Bass',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
-        name: 'Dubstep'
+        name: 'Dubstep',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
-        name: 'Electronic'
+        name: 'Electronic',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
-        name: 'Folk & Singer-Songwriter'
+        name: 'Folk & Singer-Songwriter',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
-        name: 'Hip-hop & Rap'
+        name: 'Hip-hop & Rap',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
-        name: 'House'
+        name: 'House',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
-        name: 'Indie'
+        name: 'Indie',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
-        name: 'Jazz & Blues'
+        name: 'Jazz & Blues',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
-        name: 'Latin'
+        name: 'Latin',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
-        name: 'Metal'
+        name: 'Metal',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
-        name: 'Piano'
+        name: 'Piano',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
-        name: 'Pop'
+        name: 'Pop',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
-        name: 'R&B & Soul'
+        name: 'R&B & Soul',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
-        name: 'Reggae'
+        name: 'Reggae',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
-        name: 'Reggaeton'
+        name: 'Reggaeton',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
-        name: 'Rock'
+        name: 'Rock',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
-        name: 'Soundtrack'
+        name: 'Soundtrack',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
-        name: 'Techno'
+        name: 'Techno',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
-        name: 'Trance'
+        name: 'Trance',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
-        name: 'Triphop'
+        name: 'Triphop',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
-        name: 'World'
+        name: 'World',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
     ], {});
   },
